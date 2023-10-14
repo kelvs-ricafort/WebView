64a19b9133156c49e4dba76c90bc2148336a8eec
Kelvin Ricafort <kelvs.r@outlook.com>
2023-10-14 22:14:34 +0800

    Initial commit

---
+++ app/src/main/java/com/newtechieblog/wordpress/views/webview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.newtechieblog.wordpress.views.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
